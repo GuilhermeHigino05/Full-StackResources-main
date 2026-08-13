86526651257503e27d8ae5d1ee64ddebd569a170
GuilherHigino05 <guihiginoo@gmail.com>
2026-08-12 21:58:56 -0300

    Aula02

--- Routes/TarefaRouter.js
+++ Routes/TarefaRouter.js
@@ -4,10 +4,10 @@ import TarefaController from '../Controllers/TarefaController.js'
 
 const Router = express.Router()
 let controller = new TarefaController()
-Router.get('/', controller.Read)
-Router.post('/', controller.Create)
-Router.put('/', controller.Update)
-Router.delete('/:id', controller.Delete)
-Router.get('/:id', controller.GetById)
+Router.get('/', controller.Read)/
+Router.post('/', controller.Create);
+Router.put('/', controller.Update);
+Router.delete('/:id', controller.Delete);
+Router.get('/:id', controller.GetById);
 
-export default Router
+export default Router;
